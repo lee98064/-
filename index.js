0c1474f13bb542e5632db4065d1d303486d0b340
Ryan Lee <lee98064@gmail.com>
2023-05-17 11:15:40 +0800

    🐛 Fix pen and line container position and drag error

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+let roundDecimal = function (val, precision) {
+  return (
+    Math.round(Math.round(val * Math.pow(10, (precision || 0) + 1)) / 10) /
+    Math.pow(10, precision || 0)
+  );
+};
+
 function reSize() {
   // 0.075
   let paperWidth = $("#paper-bg").width();
-  $("#pen").width(paperWidth * 0.075);
-  // $("#line").width(paperWidth * 0.075);
+  let paperHeight = $("#paper-bg").height();
+  $("#pen").css({
+    width: `${paperWidth * 0.075}px`,
+  });
+  $("#pen-container").css({
+    height: `${parseInt(paperHeight * 0.47)}px`,
+  });
+  $("#line").css({
+    width: `${paperWidth * 0.008}px`,
+  });
   let penHeight = $("#pen").height();
   $(".container").css("paddingTop", `${penHeight / 4 - 10}px`);
 }
@@ -29,7 +44,7 @@ function Snell(
 
   // 將折射角度轉換回角度
   const refractedAngle = refractedAngleRadians * (180 / Math.PI);
-  console.log(refractedAngle);
+
   return refractedAngle;
 }
 
@@ -43,45 +58,67 @@ $(function () {
   $(".pen-option input").on("change", function () {
     $(".pen-option input").each(function (index, element) {
       if ($(element).is(":checked")) {
-        $("#pen").attr("src", `./images/${$(element).val()}-long.png`);
+        $("#pen").attr("src", `./images/${$(element).val()}.png`);
       }
     });
   });
-
-  $("#pen-container").draggable({
-    handle: "#pen",
-    opacity: 0.001,
-    helper: "clone",
-    drag: function (event) {
-      var // get center of div to rotate
-        pw = document.getElementById("pen-container"),
-        pwBox = pw.getBoundingClientRect(),
-        center_x = (pwBox.left + pwBox.right) / 2,
-        center_y = (pwBox.top + pwBox.bottom) / 2,
-        // get mouse position
-        mouse_x = event.pageX,
-        mouse_y = event.pageY,
-        radians = Math.atan2(mouse_x - center_x, mouse_y - center_y),
-        degree = Math.round(radians * (180 / Math.PI) * -1 + 100);
-
-      degree += 80;
-      if (
-        !((degree >= 270 && degree <= 360) || (degree >= 0 && degree <= 91))
-      ) {
-        return;
-      }
-      var rotateCSS = `rotate(${degree}deg)`;
-      $("#pen-container").css({
-        "-moz-transform": rotateCSS,
-        "-webkit-transform": rotateCSS,
-      });
-      var refractedAngle = Snell(degree);
-      rotateCSS = `rotate(${refractedAngle}deg)`;
-      $("#line-container").css({
-        "-moz-transform": rotateCSS,
-        "-webkit-transform": rotateCSS,
-      });
-      $("#angle").text(degree);
-    },
-  });
 });
+
+var penContainer = document.getElementById("pen-container");
+var pen = document.getElementById("pen");
+var lineContainer = document.getElementById("line-container");
+var angleText = document.getElementById("angle");
+var refractedAngleText = document.getElementById("refractedAngle");
+
+var isDragging = false;
+var center_x, center_y;
+
+function handleStart(event) {
+  event.preventDefault(); // 阻止瀏覽器拖曳圖片的預設行為
+  isDragging = true;
+  var pwBox = penContainer.getBoundingClientRect();
+  center_x = (pwBox.left + pwBox.right) / 2;
+  center_y = pwBox.bottom;
+}
+
+function handleMove(event) {
+  if (isDragging) {
+    var clientX, clientY;
+
+    if (event.touches) {
+      var touch = event.touches[0];
+      clientX = touch.clientX;
+      clientY = touch.clientY;
+    } else {
+      clientX = event.clientX;
+      clientY = event.clientY;
+    }
+
+    var radians = Math.atan2(clientX - center_x, clientY - center_y);
+    var degree = Math.round(radians * (180 / Math.PI) * -1 + 100);
+    degree += 80;
+    if (!((degree >= 270 && degree <= 360) || (degree >= 0 && degree <= 91))) {
+      return;
+    }
+    var rotateCSS = "rotate(" + degree + "deg)";
+    penContainer.style.transform = rotateCSS;
+    lineContainer.style.transform = rotateCSS;
+    var refractedAngle = Snell(degree);
+    rotateCSS = "rotate(" + refractedAngle + "deg)";
+    lineContainer.style.transform = rotateCSS;
+    angleText.textContent = degree;
+    refractedAngleText.textContent = roundDecimal(refractedAngle, 2);
+  }
+}
+
+function handleEnd(event) {
+  isDragging = false;
+}
+
+pen.addEventListener("mousedown", handleStart);
+document.addEventListener("mousemove", handleMove);
+document.addEventListener("mouseup", handleEnd);
+
+pen.addEventListener("touchstart", handleStart);
+document.addEventListener("touchmove", handleMove);
+document.addEventListener("touchend", handleEnd);
